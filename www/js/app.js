987b07537adf7aae2666fee13390410960fcdc31
Ricardo Garmendia <rdgc91@gmail.com>
2017-09-20 00:29:58 -0400

    Arregle problema de persistencia con settings, mirador de archivos en Complete

--- www/js/app.js
+++ www/js/app.js
@@ -48,15 +48,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'angular-md5' ])
     }
   })
 
-   .state('app.certificate', {
-    url: '/certificate',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/certificate.html',
-        controller: 'certificateCtrl'
-      }
-    }
-  })
+ 
 
     .state('app.settings', {
     url: '/settings',
